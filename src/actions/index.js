a34c4d61d14d6a62cf4ab4981504550e85b72f73
Calla Rudolph <callarudolph@gmail.com>
2017-10-02 14:59:57 -0700

    adjust API fetch link

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,8 +10,8 @@ export function fetchForismatic() {
   return function (dispatch) {
     const forismaticId = v4();
     dispatch(requestForismatic(forismaticId));
-    return fetch("api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en")
-    .then(
+    return fetch("https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en"
+    ).then(
       response => response.json(),
       error => console.log("A Forismatic error occurred.", error)
     ).then(function(json) {
--- src/reducers/forismatic-reducer.js
+++ src/reducers/forismatic-reducer.js
@@ -2,6 +2,20 @@ import constants from './../constants';
 const { defaultState, types } = constants;
 
 const forismatic = (state = defaultState, action) => {
-  return state;
+  let newForismatic;
+  let newState;
+  switch (action.type) {
+    case types.REQUEST_FORISMATIC:
+      newForismatic = {
+        isFetching: true,
+        forismaticId: action.forismaticId
+      };
+      newState = Object.assign({}, state, {
+        [action.forismaticId]: newForismatic
+      });
+      return newState;
+    default:
+      return state;
+  }
 }
 export default forismatic;
