88cbbb7effbd438b4ab11924d3b4112c36b4b54e
Calla Rudolph <callarudolph@gmail.com>
2017-10-02 11:31:50 -0700

    add navbar styling to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Header(props) {
+  var link = {
+    paddingTop: 20,
+    paddingRight: 5
+  }
   return (
-    <div>
-      <h2>Pick Me Up</h2>
-      <div className="nav navbar-nav navbar-right">
-            <h4><Link to="/about">About</Link> | <Link to="/">Get Well</Link></h4>
-          </div>
+    <div className="navbar navbar-default">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <h2>Pick Me Up</h2>
+        </div>
+        <div style={link} className="nav navbar-nav navbar-right">
+          <p><Link to="/about">About</Link> | <Link to="/">Get Well</Link></p>
+        </div>
+      </div>
     </div>
   );
 }
