7c0576313ed91ec8214b09dd7c76a63e67354252
Calla Rudolph <callarudolph@gmail.com>
2017-10-03 13:10:15 -0700

    add modal to Input form

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchForismatic, fetchRon, fetchFoaas } from './../actions';
+import { Modal } from "react-bootstrap";
 
 class Input extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {formModalVisible: false};
+    this.showFormModal = this.showFormModal.bind(this);
+    this.hideFormModal = this.hideFormModal.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  showFormModal() {
+    this.setState({formModalVisible: true});
+  }
+
+  hideFormModal() {
+    this.setState({formModalVisible: false});
   }
 
   handleSubmit(e) {
@@ -25,18 +37,25 @@ class Input extends React.Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <p>On a scale from 1-10, how sad are you?</p>
-          <input ref="_rank"></input>
-          <br/><br/>
-          <p>Give me the name of someone or something that is currently bugging you:</p>
-          <input ref="_issue"></input>
-          <br/><br/>
-          <p>And what's yr name?</p>
-          <input ref="_name"></input>
-          <br/><br/>
-          <button>Let's do this</button>
-        </form>
+        <button onClick={this.showFormModal}>Let's do this</button>
+        <Modal
+          show={this.state.formModalVisible}
+          onHide={this.hideFormModal}>
+          <Modal.Body>
+            <form onSubmit={this.handleSubmit}>
+              <p>On a scale from 1-10, how sad are you?</p>
+              <input ref="_rank"></input>
+              <br/><br/>
+              <p>Give me the name of someone or something that is currently bugging you:</p>
+              <input ref="_issue"></input>
+              <br/><br/>
+              <p>And what's yr name?</p>
+              <input ref="_name"></input>
+              <br/><br/>
+              <button>Let's do this</button>
+            </form>
+          </Modal.Body>
+        </Modal>
       </div>
     );
   }
