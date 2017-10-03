085bfa268f235dcb051153474d9e13b888e6d3fa
Calla Rudolph <callarudolph@gmail.com>
2017-10-03 12:08:54 -0700

    add API links to About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 function About(props) {
   return (
     <div>
-      <p>About!</p>
+      <p>Not every day can be as lovely as the one where you had a banana split and got hugged by yr Momma. If things are rough out there, check in with a Pick Me Up.</p>
+      <p>Special shout out to some cool APIs:</p>
+    <ul>
+      <li><a href="https://forismatic.com/en/api/">Forismatic</a></li>
+      <li><a href="http://thecatapi.com/">The Cat API</a></li>
+      <li><a href="https://www.foaas.com/">F.O.A.A.S.</a></li>
+      <li><a href="https://github.com/jamesseanwright/ron-swanson-quotes">Ron Swanson Quotes</a></li>
+    </ul>
     </div>
   );
 }
