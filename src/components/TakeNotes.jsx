9b141ab247cd386c224216a5a92b1c80a76a65ad
Calla Rudolph <callarudolph@gmail.com>
2017-10-03 16:12:02 -0700

    create handleNewNote function with ref

--- src/components/TakeNotes.jsx
+++ src/components/TakeNotes.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 
-function TakeNotes(props) {
-  return (
-    <div>
-      <form>
-        <textarea></textarea>
-        <br/>
-        <button>Mark it</button>
-      </form>
-    </div>
-  );
+class TakeNotes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNewNote = this.handleNewNote.bind(this);
+  }
+
+  handleNewNote(e) {
+    e.preventDefault();
+    let { _note } = this.refs;
+    console.log(_note.value);
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleNewNote}>
+          <textarea ref="_note"></textarea>
+          <br/>
+          <button>Mark it</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default TakeNotes;
