519e0f62a104d8f7982a6e0bbbba4314571a60c0
Calla Rudolph <callarudolph@gmail.com>
2017-10-03 10:56:35 -0700

    condense state dot notation in display for forismatic and ron responses

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Input from './Input';
 import { connect } from 'react-redux';
 
-const Display = ({ dispatch, responses }) => {
+const Display = ({ dispatch, forismatic, ron }) => {
   let formAreaContent;
-  console.log(responses);
-  if (responses.forismatic.forismaticId === 0) {
+  const ronResponse = ron.ron;
+  if (forismatic.forismaticId === 0) {
     formAreaContent =
     <div>
       <h4>I get it, life is tough sometimes. Come on in and let's get happy.</h4>
@@ -16,7 +16,7 @@ const Display = ({ dispatch, responses }) => {
     <div>
       <h4>Sorry things are crappy today. Ron Swanson has some thoughts for you:</h4>
       <ul>
-        {responses.ron.ron.ronQuotes[0].map(function(ronQuote) {
+        {ronResponse.ronQuotes[0].map(function(ronQuote) {
           return <li key={ronQuote}>{ronQuote}</li>
         })
       }
@@ -33,9 +33,11 @@ const Display = ({ dispatch, responses }) => {
 
 const mapStateToProps = state => {
   console.log(state);
-  const responses = state;
+  const forismatic = state.forismatic;
+  const ron = state.ron;
   return {
-    responses: responses
+    forismatic: forismatic,
+    ron: ron
   };
 };
 
