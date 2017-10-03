eefa187175fcf813830f52469163b1d858ce2ad2
Calla Rudolph <callarudolph@gmail.com>
2017-10-03 16:07:44 -0700

    create TakeNotes component with textarea form

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TakeNotes from './TakeNotes';
 
 function Notes(props) {
   return (
     <div>
       <p>Soon you'll be able to take notes here.</p>
+      <TakeNotes/>
     </div>
   );
 }
---
+++ src/components/TakeNotes.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function TakeNotes(props) {
+  return (
+    <div>
+      <form>
+        <textarea></textarea>
+        <br/>
+        <button>Mark it</button>
+      </form>
+    </div>
+  );
+}
+
+export default TakeNotes;
