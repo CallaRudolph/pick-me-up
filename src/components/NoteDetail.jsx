714439366046a99c3339bc49cf2a8a5ced112132
Calla Rudolph <callarudolph@gmail.com>
2017-10-04 10:39:30 -0700

    style notes with panels

--- src/components/NoteDetail.jsx
+++ src/components/NoteDetail.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function NoteDetail(props) {
+  var time = {
+    fontSize: 10
+  }
+  
   return(
-    <div>
-      <p>{props.note}</p>
-      <p><em>({props.date})</em></p>
-      <hr/>
+    <div className="panel">
+      <p className="panel-heading">{props.note}</p>
+      <p style={time}><em>({props.date})</em></p>
     </div>
   )
 }
