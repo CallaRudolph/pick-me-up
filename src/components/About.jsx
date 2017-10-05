db429db2d7623d1dade62e45436a32b6d6a4d03f
Calla Rudolph <callarudolph@gmail.com>
2017-10-05 10:26:57 -0700

    add info about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,6 +9,11 @@ function About(props) {
     fontStyle: "italic",
     textShadow: "1px 1px black"
   }
+  var me = {
+    fontSize: 10,
+    float: "right",
+  
+  }
   return (
     <div style={about}>
       <p>Not every day can be as lovely as the one where you had a banana split and got hugged by yr Momma. If things are rough out there, check in with a Pick Me Up.</p>
@@ -20,6 +25,8 @@ function About(props) {
         <li><a style={link} href="https://github.com/jamesseanwright/ron-swanson-quotes">Ron Swanson Quotes</a></li>
         <li><a style={link} href="https://www.thedogapi.co.uk/documentation.php">The Dog API</a></li>
       </ul>
+      <br/><br/><br/>
+      <p style={me}>created by Calla Rudolph. view some of my <a style={link} href="https://github.com/CallaRudolph">projects</a> or contact me on <a style={link} href="https://www.linkedin.com/in/calla-rudolph/">LinkedIn</a></p>
     </div>
   );
 }
